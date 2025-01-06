5841efe0f2ba7a9efa6999e05516110043133491
giosantosbarbosa <giovannasantosbarbosa@hotmail.com>
2025-01-06 17:51:11 -0300

    Updates

---
+++ precache-manifest.a3ed29532299446c1d4c8cfa7f357cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e716364c03b489911a56c338b3655d7",
+    "url": "/conversordemoeda/index.html"
+  },
+  {
+    "revision": "eb1e0a1032646f8be1f9",
+    "url": "/conversordemoeda/static/css/main.4c593c69.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/conversordemoeda/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "eb1e0a1032646f8be1f9",
+    "url": "/conversordemoeda/static/js/main.5a56eff7.chunk.js"
+  },
+  {
+    "revision": "84c6ad41fbbd1e3725d0",
+    "url": "/conversordemoeda/static/js/runtime~main.b68743a5.js"
+  },
+  {
+    "revision": "de956a3201a12b8cacafb0bd38f62362",
+    "url": "/conversordemoeda/static/media/logo.de956a32.png"
+  }
+]);
